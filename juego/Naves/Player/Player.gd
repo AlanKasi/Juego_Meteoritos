e168bb69abb7f33bd3e6b5d73a79097e6ec2d175
AlanKasi <vladimirkasibrodiuk@gmail.com>
2023-10-16 20:23:41 -0300

    Meteorito
    tamaño, destruirlo y colision con nave, velocidad de rotacion aleatoria

--- juego/Naves/Player/Player.gd
+++ juego/Naves/Player/Player.gd
@@ -136,3 +136,9 @@ func recibir_danio(danio: float) -> void:
 		destruir()
 		
 	impacto_sfx.play()
+
+
+func _on_body_entered(body: Node) -> void:
+	if body is Meteorito:
+		body.destruir()
+		destruir()
